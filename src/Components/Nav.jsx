7dba565e6b7a1d28100b09ff4500a945d222af89
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-18 15:25:02 +0600

    navbar and routes created

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,84 @@
+import { Link, NavLink } from 'react-router-dom';
+
 const Nav = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to={'/'}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={'/add-product'}>Add Product</NavLink>
+      </li>
+      <li>
+        <NavLink to={'/my-cart'}>My Cart</NavLink>
+      </li>
+      <li>
+        <NavLink to={'/login'}>Login</NavLink>
+      </li>
+    </>
+  );
   return (
     <div>
-      <h2>This is Navbar</h2>
+      <div className="navbar bg-base-200">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navLinks}
+            </ul>
+          </div>
+          <Link to={'/'} className="hover:bg-gray-200 rounded-lg">
+            <img className="w-28 h-14" src="/logo.png" alt="logo" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+        </div>
+        <div className="navbar-end">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Routers/router.jsx
+++ src/Routers/router.jsx
@@ -2,6 +2,10 @@ import { createBrowserRouter } from 'react-router-dom';
 import ErrorPage from '../Pages/Errorpage';
 import Root from '../layout/Root';
 import Home from '../Pages/Home';
+import Login from '../Pages/Login';
+import AddProduct from '../Pages/AddProduct';
+import MyCart from '../Pages/MyCart';
+
 const router = createBrowserRouter([
   {
     path: '/',
@@ -11,6 +15,18 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home></Home>,
+      },
+      {
+        path: '/add-product',
+        element: <AddProduct></AddProduct>,
+      },
+      {
+        path: '/my-cart',
+        element: <MyCart></MyCart>,
+      },
+      {
+        path: '/login',
+        element: <Login></Login>,
       },
     ],
   },
