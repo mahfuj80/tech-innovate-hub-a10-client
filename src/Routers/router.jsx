269a6c4ba89fde759bdf2ebf5acf987d4f44fb50
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-18 12:59:26 +0600

    error element added and children created

--- src/Routers/router.jsx
+++ src/Routers/router.jsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
+import ErrorPage from '../Pages/Errorpage';
+import Root from '../layout/Root';
+import Home from '../Pages/Home';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
